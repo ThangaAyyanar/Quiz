7c26c43fbdc83120b7200a22667af836d944aeab
Thanga Ayyanar <gldayan8@gmail.com>
2017-04-17 21:22:04 +0530

    now save score to db

--- ques.php
+++ ques.php
@@ -1,5 +1,7 @@
 <?php
+  session_start();
   require 'count.php';
+  require 'db_connect.php';
   $xml=simplexml_load_file("file.php");
   $arr = array();
   $cnt=$_POST['cont'];
@@ -26,11 +28,22 @@
       $cnt1=$cnt1+1;
     }
     //echo $arr[$i]<=>$str[$i];
-    echo $arr[$i]."-----".$str[$i]."<br>";
+    //echo $arr[$i]."-----".$str[$i]."<br>";
   }
-  echo "Score out of ".($cnt)." is ".$cnt1;
-  echo "<br><br>Reasons for the Answer<br><br>";
+  if(!isset($_SESSION['mark'])){
+	  $_SESSION['mark']=$cnt1;
+  }
+  $sql="INSERT INTO marks VALUES('".$_SESSION["user"]."','".$_SESSION['mark']."')";
+  if(mysqli_query($conn,$sql)){
+  echo "<br><br><br><h1 align='center'>Hello<b style='color:slateblue'> ".$_SESSION["user"]."</b> ";//<a href='logout.php'>Logout</a></h1>";
+  echo "<br><br><br><h1 align='center'>Your score out of <b style='color:brown'>".($cnt)."</b> is <b style='color:green'>".$_SESSION['mark']."</b></h1>";
+  
+  echo "<br><br><br><h1 align='center'>Thank you</h1>";
+  }else{
+	echo "<br><br><br><h1 align='center'>Error ".mysqli_error($conn)."</h1>";
+  }
+  /*echo "<br><br><br><b style='color:green'>Reasons for the Answer</b><br><br>";
   for($i=0;$i < $cnt;$i++){
-    echo ($i+1).")Reason : ".$xml->answer[$i]->rea."<br>";
-  }
+    echo ($i+1).")<b>Reason</b> : ".$xml->answer[$i]->rea."<br>";
+  }*/
  ?>
