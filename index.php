bd5b5881162a5cba14f656dcf070a4e99d3b46bc
Thanga Ayyanar <gldayan8@gmail.com>
2017-04-17 21:19:31 +0530

    index for quiz

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/bootstrap.min.css"/>
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<title>Quiz for US</title>
+</head>
+<body>
+  <div class="container">
+	<div class="page-header">
+		<h1 style="color:chocolate">Welcome to Quiz For Us</h1>
+	</div>
+	<p>
+		Ready to Enter into quiz world enter your register number click the below button <br>
+        <form action="testques.php" method="post">
+	<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><input type="text" size="36px" name="user" maxlength="12" placeholder="Enter your register number" required/></div>
+		<div class="col-sm-4"></div>
+	</div>
+	<br>
+	<div class="row">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4"><input type="submit" class="btn btn-success btn-block" value="Let's Go"></div>
+		<div class="col-sm-4"></div>
+	</div>
+	</form>
+	</p>
+		<blockquote align="center">"Sweat More in Practice , Bleed less in War"</blockquote>
+
+  </div>
+</body>
+</html>
